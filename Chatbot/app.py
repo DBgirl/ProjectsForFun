2c2367d0ff205542fdb890a6c6a36955be0fa9ad
Uva <uva.jagadeesan@bp.com>
2022-10-31 20:09:48 +0530

    adding key file

--- Chatbot/app.py
+++ Chatbot/app.py
@@ -5,21 +5,13 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 import json
 from urllib import parse
 import urllib.request
+import keys
 #import pandas as pd
 
 app = Flask(__name__,template_folder='templates')
 
 
 url = "http://api.giphy.com/v1/gifs/search"
-
-params = parse.urlencode({
-  "q": "That isn't my name",
-  "api_key": "GMEINSldKQkIZIiRTKCvk08crSNGZWTl",
-  "limit": "1"
-})
-
-
-
 
 #create chatbot
 #englishBot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
@@ -40,7 +32,7 @@ def get_bot_response():
     userText = request.args.get('msg') #get input message
     botreply =str(englishBot.get_response(userText))
     print (botreply)
-    params = parse.urlencode({  "q": {botreply[0:45]}, "api_key": "GMEINSldKQkIZIiRTKCvk08crSNGZWTl",  "limit": "1"}) #call API
+    params = parse.urlencode({  "q": {botreply[0:45]}, "api_key": keys.giphy,  "limit": "1"}) #call API
 
     with urllib.request.urlopen("".join((url, "?", params))) as response:
       data = json.loads(response.read())
